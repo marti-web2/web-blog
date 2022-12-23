e6af3b55102565a1bf41b7813e990f1bfd642c83
David Martinez <anddagger@gmail.com>
2022-12-23 16:15:12 -0600

    connected to mongodb

--- backend/app.js
+++ backend/app.js
@@ -1,21 +1,16 @@
 import express from "express"
 import mongoose from "mongoose"
-require('dotenv').config()
+import dotenv from "dotenv"
+
+dotenv.config()
 
 const app = express()
 const PORT = 5000
-const mongoKey = process.env.MONGO_KEY
+const mongoPassword = process.env.MONGO_PASSWORD
 
 mongoose.connect(
-  `mongodb+srv://marti:${mongoKey}@cluster0.4k3mknc.mongodb.net/Blog?retryWrites=true&w=majority`, { 
-    useNewUrlParser: true, useUnifiedTopology: true 
-  })
+  `mongodb+srv://marti:${mongoPassword}@cluster0.4k3mknc.mongodb.net/?retryWrites=true&w=majority`
+  )
 .then(() => app.listen(PORT))
 .then(() => console.log(`Connected to Database and listening to LocalHost:${PORT}`))
 .catch((err) => console.log(err))
-
-app.use("/", (req,res,next) => {
-  res.send("Hello World")
-})
-
-app.listen(PORT)
