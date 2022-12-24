79ccba603f556a4c6b3e7f21ae790341504b54ed
David Martinez <anddagger@gmail.com>
2022-12-23 18:07:22 -0600

    created login request

--- backend/controllers/user-controller.js
+++ backend/controllers/user-controller.js
@@ -1,4 +1,5 @@
 import User from "../model/User"
+import bcrypt from "bcryptjs"
 
 export const getAllUsers = async (req, res, next) => {
   let users
@@ -11,22 +12,70 @@ export const getAllUsers = async (req, res, next) => {
 }
 
 export const signup = async (req, res, next) => {
-  const { name, email, password } = req.body
-  let existingUser
+  const { name, email, password } = req.body;
+  let existingUser;
+
   try {
-    existingUser = await User.findOne({ email })
-    // asynchronously create a new user if it doesn't exist
-    if (!existingUser) {
-      const user = new User({
-        name,
-        email,
-        password,
-      })
-      await user.save()
-      return res.status(201).json({ message: "User created!" })
-    }
+    existingUser = await User.findOne({ email });
   } catch (error) {
-    return res.status(500).json({ message: "User creation failed!" })
+    return res.status(500).json({ message: "User creation failed!" });
   }
-  return res.status(422).json({ message: "User already exists! Login instead." })
-}
+
+  if (existingUser) {
+    return res.status(422).json({ message: "User already exists! Login instead." });
+  }
+
+  let hashedPassword;
+  try {
+    hashedPassword = await bcrypt.hash(password, 12);
+  } catch (error) {
+    return res.status(500).json({ message: "User creation failed!" });
+  }
+
+  const user = new User({
+    name,
+    email,
+    password: hashedPassword,
+  });
+
+  try {
+    await user.save();
+  } catch (error) {
+    return res.status(500).json({ message: "User creation failed!" });
+  }
+
+  return res.status(201).json({ message: "User created!" });
+};
+
+
+export const login = async (req, res, next) => {
+  const { email, password } = req.body;
+  let existingUser;
+
+  try {
+    existingUser = await User.findOne({ email });
+  } catch (error) {
+    return res.status(500).json({ message: "Login failed!" });
+  }
+
+  if (!existingUser) {
+    return res.status(404).json({ message: "User with this email was not found!" });
+  }
+
+  let isPasswordCorrect;
+  try {
+    isPasswordCorrect = await bcrypt.compare(password, existingUser.password);
+  } catch (error) {
+    return res.status(500).json({ message: "Login failed!" });
+  }
+
+  if (!isPasswordCorrect) {
+    return res.status(401).json({ message: "Invalid credentials!" });
+  }
+
+  return res.status(200).json({ message: "Logged in was successful!" });
+};
+
+
+/* Consider using a specialized library or service for handling user authentication and authorization, 
+such as Passport.js or Auth0. */
