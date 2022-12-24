6120e64b85e54251b1b0a6038a03580b36de9b3c
David Martinez <anddagger@gmail.com>
2022-12-23 20:00:18 -0600

    added blog controller + routes

--- backend/controllers/blog-controller.js
+++ backend/controllers/blog-controller.js
@@ -9,3 +9,19 @@ export const getAllBlogs = async (req, res, next) => {
     return res.status(404).json({ message: "No blogs found!" })
   }
 }
+
+export const addBlog = async (req, res, next) => {
+  const { title, description, image, user } = req.body
+  const blog = new Blog({
+    title,
+    description,
+    image,
+    user
+  })
+  try {
+    await blog.save()
+    return res.status(201).json({ blog })
+  } catch (error) {
+    return res.status(409).json({ message: "Blog not saved!" })
+  }
+}
