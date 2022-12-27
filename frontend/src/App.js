eb9bf3ffe700c3b126758f7a7b9c0f87042cf412
Marti <marti@martimcdee.com>
2022-12-27 03:39:52 +0000

    set values for text field props on auth page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,24 @@
-import React, { useState } from "react"
-import { Routes, Route } from "react-router-dom"
-import Auth from "./components/Auth"
-import Blogs from "./components/Blogs"
-import UserBlogs from "./components/UserBlogs"
-import BlogDetail from "./components/BlogDetail"
-import AddBlog from "./components/AddBlog"
-import Header from "./components/Header"
+import Header from "./components/Header";
+import Blogs from "./components/Blogs";
+import UserBlogs from "./components/UserBlogs";
+import BlogDetail from "./components/BlogDetail";
+import AddBlog from "./components/AddBlog";
 
-const App = () => {
-  const isLoggedIn = useSelector(state => state.isLoggedIn)
+import React, { useEffect } from "react";
+import { Route, Routes } from "react-router-dom";
+import Auth from "./components/Auth";
+import { useDispatch, useSelector } from "react-redux";
+import { authActions } from "./store";
+function App() {
+  const dispatch = useDispatch();
 
+  const isLoggedIn = useSelector((state) => state.isLoggedIn);
+  console.log(isLoggedIn);
+  useEffect(() => {
+    if (localStorage.getItem("userId")) {
+      dispatch(authActions.login());
+    }
+  }, [dispatch]);
   return (
     <React.Fragment>
       <header>
@@ -17,16 +26,20 @@ const App = () => {
       </header>
       <main>
         <Routes>
-          <Route path="/auth" element={<Auth />} />
-          <Route path="/blogs" element={<Blogs />} />
-          <Route path="/myBlogs" element={<UserBlogs />} />
-          <Route path="/myBlogs/:id" element={<BlogDetail />} />
-          <Route path="/blogs/add" element={<AddBlog />} />
+          {!isLoggedIn ? (
+            <Route path="/auth" element={<Auth />} />
+          ) : (
+            <>
+              <Route path="/blogs" element={<Blogs />} />
+              <Route path="/blogs/add" element={<AddBlog />} />
+              <Route path="/myBlogs" element={<UserBlogs />} />
+              <Route path="/myBlogs/:id" element={<BlogDetail />} />{" "}
+            </>
+          )}
         </Routes>
       </main>
-
     </React.Fragment>
-  )
+  );
 }
 
-export default App
+export default App;
